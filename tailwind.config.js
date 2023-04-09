c6a44270f6f1d0f69d807f7b3e032f51037e8ea3
Ivan Di Paolo <ivan.dipaolo@meteologica.com>
2023-04-09 20:53:58 +0200

    feature: modified base survey data, added font, addded to gitignore vscode configs
    Task #000021

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,10 @@ module.exports = {
   theme: {
     extend: {},
   },
+  fontFamily: {
+    sans: ['Inter var', 'ui-sans-serif'],
+    serif: ['Merriweather', 'serif'],
+  },
   plugins: [],
   darkMode: "class"
 }
